919958bc1bad19d77b1a6287be7159a1584951f4
i47uen <jeealay8@gmail.com>
2020-08-10 03:52:15 +0700

    fin hero slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#hero-slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    nextArrow: '<button type="button" class="slick-next"> ᐳ </button>',
+    prevArrow: '<button type="button" class="slick-prev"> ᐸ </button>'
+});
