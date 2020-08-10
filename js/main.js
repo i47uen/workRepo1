b24f6bdcb9911a5f52db6298ae83fc9c6a6be469
i47uen <jeealay8@gmail.com>
2020-08-10 14:15:07 +0700

    update

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@ $('#hero-slider').slick({
     nextArrow: '<button type="button" class="slick-next"> ᐳ </button>',
     prevArrow: '<button type="button" class="slick-prev"> ᐸ </button>'
 });
+
+$(document).ready(function () {
+    /**
+     * При прокрутке страницы, показываем или срываем кнопку
+     */
+
+    $('#button-up').hide()
+
+    $(window).scroll(function () {
+
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 950) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+        console.log($(this).scrollTop());
+    });
+
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+});
